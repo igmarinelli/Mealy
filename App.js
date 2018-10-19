5ff1da06607095970c13d08b0998b475fc94acad
FredOliveiraSantos <oliveira.fredsantos@gmail.com>
2018-10-18 19:54:58 -0700

    Adding Firebase test and Google Maps to Map Icon

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Animated, Image, StyleSheet, Text, View, Easing, Dimensions } from 'react-native';
+import { Animated, Image, StyleSheet, Text, View, Easing, Dimensions, Linking } from 'react-native';
 import { LinearGradient, Font, AppLoading } from 'expo';
 import { Button, Card, Icon} from 'react-native-elements';
+
 
 export default class App extends Component {
 
@@ -9,8 +10,21 @@ export default class App extends Component {
     super(props);
     this.state = {
     	isReady: false,
-      imageHW: new Animated.Value(300)
+      imageHW: new Animated.Value(300),
+      locationText: "Sproul Hall @ 1:30 P.M."
     }
+  }
+
+  reserveButtonClicked() {
+    //1. Check if user logged in
+    //2. Log in using Google
+    // Dialog asking amount
+    // Options: pay with cash or pay with Venmo/Paypal
+  }
+
+  locationIconClicked() {
+    var locOnly = this.state.locationText.split('@')[0];
+    Linking.openURL("http://maps.google.com/maps?q=" + locOnly.replace(' ','+'));
   }
 
   componentDidMount() {
@@ -37,6 +51,7 @@ export default class App extends Component {
   this.setState({isReady:true})
 }
 
+
   render() {
     if (!this.state.isReady) {
       return <AppLoading />;
@@ -52,16 +67,47 @@ export default class App extends Component {
 	      	<View style={{flex: 2}}>
 	      		<Text style={{color: '#fff', fontSize: 40, textAlign: 'center'}}>$5 Beef with rice</Text>
 	      		<View style={{flexDirection: 'row', justifyContent: 'center'}}>
-	      			<Text style={{color: '#fff', fontSize: 20, textAlign: 'center'}}>Sproul Plaza 1:30 p.m.</Text><Icon name='location-on' color='white' />
+              <Text 
+                style={{color: '#fff', fontSize: 20, textAlign: 'center'}}> {this.state.locationText} 
+              </Text>
+              <Icon name='location-on' color='white' onPress={this.locationIconClicked.bind(this)} />
 	      		</View>
 	      	</View>
 	      	<View style={{flex: 2}}>
-	      		<Button large fontWeight='bold' rounded={true} title='Quick Reservation' backgroundColor='#fdbf2e'/>
+            <Button large fontWeight='bold' rounded={true} title='Reserve Now!' backgroundColor='#fdbf2e' onPress={this.reserveButtonClicked}/>
 	      	</View>
       </LinearGradient>
     );
   }
 }
+
+var firebase = require("firebase");
+
+var config = {
+  authDomain: "californiafoodapp.firebaseapp.com",
+  databaseURL: "https://californiafoodapp.firebaseio.com",
+  projectId: "californiafoodapp",
+  storageBucket: "californiafoodapp.appspot.com",
+  messagingSenderId: "1034070381072"
+};
+
+if(!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+function saveReservation(email, timestamp){
+  //Save JSON of string
+  firebase.database().ref("Client Reservations/").set({
+    email,
+    timestamp
+  }).then((data) => {
+    console.log("data", data);
+  }).catch((error) => {
+    console.log("error", error)
+  })
+}
+
+saveReservation("fredtest00@berkeley.edu", new Date().getDate());
 
 const styles = StyleSheet.create({
   container: {
